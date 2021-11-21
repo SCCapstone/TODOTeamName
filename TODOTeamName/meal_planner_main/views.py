96bef9bee6871fbe28fd52a686d860cbfa5538ab
Garrison <garrison.davis1@gmail.com>
2021-11-21 16:58:55 -0500

    Added Health forum & basic grocery list

--- TODOTeamName/meal_planner_main/views.py
+++ TODOTeamName/meal_planner_main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-	return HttpResponse("welcome to the home page!")
+	return HttpResponse(request, 'homePage.html')
+
+def groceryListView(request):
+	return render(request, 'groceryListMain.html')
