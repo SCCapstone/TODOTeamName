758be6525a015e1253480a04e87bcc45a7fafe35
Isaac Luther <iluther@email.sc.edu>
2021-11-28 20:39:17 -0500

    database commit

--- TODOTeamName/meal_planner_main/models.py
+++ TODOTeamName/meal_planner_main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+        Username = models.
+
+class Recipe(models.Model):
+        recipe_name = models.CharField(max_length=200)
+        recipe_ingredients = 
+        date_added = models.DateTimeField('date published')
+        #TODO- add user to recipe?
+        #TODO- allergy flags?
+
+class forumPost(models.Model):
+        #TODO-add user to database
+        title = models.CharField(max_length=200)
+        intro = models.CharField(max_length=200)
+        date_added = models.DateTimeField('date published')
+
+class groceryItems(models.Model):
+        name = models.CharField(max_length=50)
