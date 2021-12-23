69cff9ec648c164e0619e33e82ac9566787c04d6
Carol <cfjuneau@live.com>
2021-12-23 13:16:16 -0500

    moved forms into each app

---
+++ TODOTeamName/cal/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm, DateInput
+
+from .models import *
+
+class ScheduledRecipeForm(ModelForm):
+    class Meta:
+        model = ScheduledRecipe
+        widgets = {
+            'scheduled_date': DateInput(attrs={'type': 'date'}),
+        }
+        fields = '__all__'
---
+++ TODOTeamName/pantry/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+
+from .models import *
+
+class PantryAddItemForm(forms.ModelForm):
+    name = forms.CharField(max_length=50)
+    expiration = forms.DateField()
+
+    class Meta:
+        model = pantryItems 
+        fields = ['name', 'expiration']
