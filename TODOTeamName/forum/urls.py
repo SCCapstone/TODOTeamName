fa9d3d7da7339c8861202770ffee4d40cbc36dac
Carol <cfjuneau@live.com>
2021-12-23 12:00:29 -0500

    moved urls into each app

---
+++ TODOTeamName/forum/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+
+from . import views 
+
+app_name = 'forum'
+urlpatterns = [
+    # path('', views.frontpage, name = 'forumMain'),
+    # path('post/', views.forumPost, name = 'fpost'),
+    # path('<slug:slug>/',post_detail, name ='post_detail'),
+]
---
+++ TODOTeamName/pantry/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+
+from . import views 
+
+app_name = 'pantry'
+urlpatterns = [
+    # path('', views.pantry, name = 'pantryMain'),
+    # path('addPantryItem/', views.addPantryItem),
+]
