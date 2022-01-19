eaa347100eb38f21155099a9d48d1a0f6173d225
Isaac Luther <iluther@email.sc.edu>
2022-01-19 09:48:11 -0500

    Make database scripts run in elasticbeanstalk

--- TODOTeamName/todo_team_name/settings.py
+++ TODOTeamName/todo_team_name/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,13 +82,23 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pgdb',
-        'USER': 'admin',
-        'PASSWORD': 'password',
+        'USER': 'fourninetyadmin',
+        'PASSWORD': 'fourninetypassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
