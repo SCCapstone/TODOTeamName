5bf3be3054f457c8ea74e13bfca48eab8662beb7
Matthew Lewis <matthew.r.lewis1@gmail.com>
2022-02-15 16:37:25 -0500

    updated to allow for saving in api search function, not yet tied to user

--- TODOTeamName/recipes/views.py
+++ TODOTeamName/recipes/views.py
@@ -21,7 +21,7 @@ def recipes(request):
     return render(request, 'recipesMain.html', {'list': json.loads(response.text)})
 
 def rsearch(request):
-    if request.method == "POST":
+if request.method == "POST":
         ingredients = request.POST.get("search")
         querystring = {"query":ingredients,"offset":"0","number":"1"}
         response ={'list': json.loads(requests.request("GET", url, headers=headers, params=querystring).text)}
@@ -29,8 +29,12 @@ def rsearch(request):
         for i in response['list']['results']:
             id=id+str(i['id'])+","
         ressearch = {"ids":str(id)}
-        context = {'list': json.loads(requests.request("GET", url2, headers=headers, params=ressearch).text)}
-        return render(request, "apisearch.html", context)        #TODO figure out the save function
+        context = {'list': json.loads(requests.request("GET", url2, headers=headers, params=ressearch).text),'ingredients':ingredients}
+        if request.POST.get('save')!=None:
+            i = int(request.POST.get('save'))-1
+            recipe = saverecipeapi(context['list'][i])
+            return render(request, 'prove.html', recipe) #TODO tie to save user not prove.html 
+        return render(request, "apisearch.html",  context)
 
     else:
         return render(request, "apisearch.html")
@@ -41,22 +45,26 @@ def rcreate(request):
         maketime = request.POST.get("maketime")
         ingredients = request.POST.get("ingredients")
         steps = request.POST.get("steps")
-        recipe = {"title":title, "maketime":maketime, "ingredients":ingredients, "steps":steps, "creator":"me"} #TODO insert username
+        recipe = {"title":title, "maketime":maketime, "ingredients":ingredients, "steps":steps, "creator":request.user.username} #TODO insert username
+        #TODO save to user
         return render(request, 'prove.html', recipe) # TODO redirect back to my recipes
     else:
         return render(request, 'recipecreation.html')
 
 def rview(request):
-    return render (request, 'recipeview.html' ,user.recipes)
+    return render (request, 'recipeview.html', user.recipes)
 
 def redit(request):
+    recipe = {"title":"make it", "maketime":"3 years", "ingredients":"test123", "steps":"test test test", "creator":"me"}
     if request.method == "POST":
         title = request.POST.get("title")
         maketime = request.POST.get("maketime")
         ingredients = request.POST.get("ingredients")
         steps = request.POST.get("steps")
-        if recipe['creator']!="me":
-           creator = creator+"\n edited by "+"me" #TODO insert username
+        if recipe['creator']!=request.user.username:
+           creator = recipe['creator']+"\n edited by "+request.user.username #TODO insert username
+        else:
+            creator = recipe['creator']
         recipe = {"title":title, "maketime":maketime, "ingredients":ingredients, "steps":steps, "creator":creator}
         return render(request, 'prove.html', recipe)
     else:
