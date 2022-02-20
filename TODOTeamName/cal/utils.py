381f5a17d75a1fa1f611464924c7df96567581a1
caroljuneau <cfjuneau@live.com>
2022-02-20 10:27:40 -0500

    fix formatting

--- TODOTeamName/cal/utils.py
+++ TODOTeamName/cal/utils.py
@@ -1,7 +1,7 @@
 from calendar import HTMLCalendar, month_name
 from .models import ScheduledRecipe
 from django.urls import reverse
-import datetime 
+import datetime
 
 
 class Calendar(HTMLCalendar):
@@ -12,12 +12,14 @@ class Calendar(HTMLCalendar):
         self.setfirstweekday(6)
 
     def formatday(self, day, scheduled_recipes, active_user):
-        food_by_day = scheduled_recipes.filter(scheduled_date__day=day).filter(user=active_user)
+        food_by_day = scheduled_recipes.filter(
+            scheduled_date__day=day).filter(user=active_user)
         d = ''
         for scheduled_recipe in food_by_day:
             d += f'<li>{ scheduled_recipe.get_html_url }</li>'
         if day != 0:
-            url = reverse('cal:calDay') + f'?day={ self.year }-{ self.month }-{ day }'
+            url = reverse('cal:calDay') + \
+                f'?day={ self.year }-{ self.month }-{ day }'
             return f'<td><a class="btn btn-light btn-sm date" href="{url}">{ day }</a><ul>{ d }</ul></td>'
         return '<td></td>'
 
@@ -30,7 +32,6 @@ class Calendar(HTMLCalendar):
     def formatmonth(self, active_user, withyear=True):
         scheduled_recipes = ScheduledRecipe.objects.filter(
             scheduled_date__year=self.year, scheduled_date__month=self.month)
-
         cal = f'<table class="calendar" border="0" cellpadding="0" cellspacing="0">\n'
         cal += f'{ self.formatmonthname(self.year, self.month, withyear=withyear) }\n'
         cal += f'{ self.formatweekheader() }\n'
@@ -44,58 +45,52 @@ class Calendar(HTMLCalendar):
 
 class WeekCalendar(HTMLCalendar):
     def __init__(self, year=None, week=None):
-        self.year = year 
-        self.week = week 
+        self.year = year
+        self.week = week
         super(WeekCalendar, self).__init__()
         self.setfirstweekday(6)
 
     def formatday(self, date, scheduled_recipes):
-        food_by_day = list(filter(lambda x:
-        (x.scheduled_date.day == date.day), scheduled_recipes))
+        food_by_day = list(
+            filter(lambda x: (x.scheduled_date.day == date.day), scheduled_recipes))
         d = ''
         for scheduled_recipe in food_by_day:
             d += f'<li>{ scheduled_recipe.get_html_url }</li>'
-        url = reverse('cal:calDay') + f'?day={ self.year }-{ date.month }-{ date.day }'
+        url = reverse('cal:calDay') + \
+            f'?day={ self.year }-{ date.month }-{ date.day }'
         return f'<td><a class="btn btn-light btn-sm date" href="{ url }">{ date.month }-{ date.day }</a><ul>{ d }</ul></td>'
-    
+
     def formatweek(self, active_user):
-        scheduled_recipes = list(filter(lambda x: 
-            (x.user == active_user) and 
-            (x.scheduled_date.year == self.year) and 
-            (x.scheduled_date.isocalendar()[1] == self.week), 
-            ScheduledRecipe.objects.all()))
+        scheduled_recipes = list(filter(lambda x: (x.user == active_user) and (x.scheduled_date.year == self.year) and (
+            x.scheduled_date.isocalendar()[1] == self.week), ScheduledRecipe.objects.all()))
         cal = f'<table class="calendar" border="0" cellpadding="0" cellspacing="0">\n'
         cal += f'{ self.year } Week { self.week }\n'
         cal += f'{ self.formatweekheader() }\n'
         cal += f'<tr>'
         start_date = datetime.date.fromisocalendar(self.year, self.week, 1)
         end_date = start_date + datetime.timedelta(days=6.9)
-        current_date = start_date 
+        current_date = start_date
         while current_date <= end_date:
             cal += self.formatday(current_date, scheduled_recipes)
             current_date += datetime.timedelta(days=1)
         cal += f'</tr>'
-        return cal 
+        return cal
 
 
 class DayCalendar(HTMLCalendar):
     def __init__(self, year=None, month=None, day=None):
-        self.year = year 
-        self.month = month 
-        self.day = day 
+        self.year = year
+        self.month = month
+        self.day = day
         super(DayCalendar, self).__init__()
-    
+
     def formatday(self, active_user):
-        scheduled_recipes = list(filter(lambda x: 
-            (x.user == active_user) and 
-            (x.scheduled_date.year == self.year) and 
-            (x.scheduled_date.month == self.month) and
-            (x.scheduled_date.day == self.day),
-            ScheduledRecipe.objects.all()))
+        scheduled_recipes = list(filter(lambda x: (x.user == active_user) and (x.scheduled_date.year == self.year) and (
+            x.scheduled_date.month == self.month) and (x.scheduled_date.day == self.day), ScheduledRecipe.objects.all()))
         cal = f'<table class="calendar" border="0" cellpadding="0" cellspacing="0">\n'
         cal += f'<tr><td><span class="date">{ month_name[self.month] } { self.day }, { self.year }</span>\n'
         cal += f'<ul>'
         for scheduled_recipe in scheduled_recipes:
-            cal += f'<li>{ scheduled_recipe.get_html_url }</li>'
+            cal += f'<li>{ scheduled_recipe.get_html_url }<p>{ scheduled_recipe.description }</p></li>'
         cal += f'</ul></td></tr>'
-        return cal 
+        return cal
