b6151e779dc71b85158773dae96df92d74a9e32e
Carol <cfjuneau@live.com>
2022-02-27 13:07:08 -0500

    can delete scheduled recipes

--- TODOTeamName/cal/urls.py
+++ TODOTeamName/cal/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('weekview', views.WeekView.as_view(), name='calWeek'),
     path('dayview', views.DayView.as_view(), name='calDay'),
     path('scheduled_recipe/new/', views.scheduled_recipe,
-         name='scheduled_recipe_new'),
+        name='scheduled_recipe_new'),
     path('scheduled_recipe/edit/<int:scheduled_recipe_id>/',
-         views.scheduled_recipe, name='scheduled_recipe_edit'),
-    path('scheduled_recipe/view/<int:scheduled_recipe_id>/',
-         views.scheduled_recipe, name='scheduled_recipe_view'),
+        views.scheduled_recipe, name='scheduled_recipe_edit'),
+    path('scheduled_recipe/delete/<int:pk>/',
+        views.DeleteView.as_view(), name='scheduled_recipe_delete')
 ]
