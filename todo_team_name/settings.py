412211c3cd0f54fd0767a43f40117e6e824fb829
Isaac Luther <iluther@email.sc.edu>
2022-02-27 13:27:18 -0500

    forum migrations

--- todo_team_name/settings.py
+++ todo_team_name/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-h)-jo=v_z1ls%tf*$wsyo^5c^yy!6w!ve204=)#k&&mp41y1#('
+SECRET_KEY='4fWriyVvAzK0yOgwdpoS7XLlIXuWuEMRKP5W6IBTGn4='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
