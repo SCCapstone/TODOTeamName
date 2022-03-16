513740ef2059b66eee8ccd4a06afd06421b920ae
Isaac Luther <iluther@email.sc.edu>
2022-03-16 18:28:31 -0400

    Add following-only page to forum

--- forum/urls.py
+++ forum/urls.py
@@ -8,6 +8,7 @@ app_name = 'forum'
 urlpatterns = [
     path('', frontpage, name='healthForumMain'),
     path('user/<uName>', profilepage, name='userPage'),
+    path('following/', followingpage, name='followingPage'),
     path('post/', valid_post, name = 'valid_post'),
     path('imgpost/', valid_image_post, name = 'valid_image_post'),
     path('<slug:slug>/', post_detail, name = 'post_detail'),
