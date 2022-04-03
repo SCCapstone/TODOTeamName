dd6379a5787c05fcb20e2aa1af30ca65456b0e24
Isaac Luther <iluther@email.sc.edu>
2022-04-03 14:30:20 -0400

    Add send to pantry functionality, fix delete in pantry, grocery

--- grocery/views.py
+++ grocery/views.py
@@ -5,19 +5,21 @@ from dal import autocomplete
 
 from .models import *
 from .forms import *
+from pantry.models import pantryItems
 
 
 @login_required
 def groceryListMain(request):
+    all_grocery_items = groceryItems.objects.filter(user=request.user)
     if request.method == 'POST':
         form = GroceryItemsForm(request.POST)
-        if request.POST.get('edit')!=None:
-            i = int(request.POST.get("edit"))
-            return redirect("/grocery/editGroceryItem")
-        elif request.POST.get('delete')!=None:
-            i=int(request.POST.get('delete'))
-            all_grocery_items[i-1].delete()
-        elif form.is_valid():
+        #if request.POST.get('edit')!=None:
+        #    i = int(request.POST.get("edit"))
+        #    return redirect("/grocery/editGroceryItem")
+        #elif request.POST.get('delete')!=None:
+        #    i=int(request.POST.get('delete'))
+        #    all_grocery_items[i-1].delete()
+        if form.is_valid():
             if not groceryItems.objects.filter(user = request.user, item_name = form.instance.item_name):
                 form.instance.user = request.user
                 form.save()
@@ -28,8 +30,28 @@ def groceryListMain(request):
             return redirect('grocery:groceryMain')
     else:
         form = GroceryItemsForm()
-    all_grocery_items = groceryItems.objects.filter(user=request.user)
     return render(request, 'grocery/groceryListMain.html', {'grocery_page': 'active', 'all_grocery_items': all_grocery_items, 'form': form})
+
+def editdelete(request):
+    all_grocery_items = groceryItems.objects.filter(user=request.user)
+    if request.method == 'POST':
+        if request.POST.get('edit')!=None:
+            i = int(request.POST.get("edit"))
+            return redirect("/grocery/editGroceryItem")
+        elif request.POST.get('delete')!=None:
+            i=int(request.POST.get('delete'))
+            all_grocery_items[i-1].delete()
+        elif request.POST.get('sendtopantry')!=None:
+            i = int(request.POST.get('sendtopantry'))
+            g = all_grocery_items[i-1]
+            if not pantryItems.objects.filter(user = request.user, name = g.item_name):
+                p = pantryItems.objects.create(name = g.item_name, quantity = g.quantity, user = g.user)
+            else:
+                item = pantryItems.objects.get(user = request.user, name = g.item_name)
+                item.quantity = item.quantity + g.quantity
+                item.save()
+            
+    return redirect('grocery:groceryMain')
 
 
 #@login_required
